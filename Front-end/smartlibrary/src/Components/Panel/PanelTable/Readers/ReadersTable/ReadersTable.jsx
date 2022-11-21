2c3a556d0e9e9494e263790bc1ee8bc642b93ba6
KirillBazavlutskiy <bazik345@gmail.com>
2022-11-21 14:20:04 +0200

    added state

--- Front-end/smartlibrary/src/Components/Panel/PanelTable/Readers/ReadersTable/ReadersTable.jsx
+++ Front-end/smartlibrary/src/Components/Panel/PanelTable/Readers/ReadersTable/ReadersTable.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import s from './ReadersTable.module.css';
+import QRCode from 'react-qr-code';
 
 const ReadersTable = () => {
 	return (
 		<div className={s.container}>
-			<h1>hi</h1>
+			<div className={s.header}>
+				<p>ПІБ</p>
+				<p>Email</p>
+				<p>Телефон</p>
+				<p>QR-код</p>
+				<p>Статус</p>
+			</div>
+			<div className={s.main}></div>
 		</div>
 	);
 };
