95a75163cbbdb3255a888a9e5f0ae877b4ea3859
KirillBazavlutskiy <bazik345@gmail.com>
2022-11-21 14:56:38 +0200

    added props again

--- Front-end/smartlibrary/src/Pages/AdminMainPage/AdminPage.jsx
+++ Front-end/smartlibrary/src/Pages/AdminMainPage/AdminPage.jsx
@@ -3,25 +3,14 @@ import Calendar from '../../Components/Calendar/Calendar';
 import Panel from '../../Components/Panel/Panel';
 import s from './AdminPage.module.css';
 
-<<<<<<< HEAD
 const AdminPage = props => {
 	return (
 		<div className={s.container}>
+			<Header />
 			<Calendar />
 			<Panel tables={props.admin.tables} />
 		</div>
 	);
 };
-=======
-const AdminPage = () => {
-    return(
-        <div className={s.container}>
-            <Header/>
-            <Calendar/>
-            <Panel/>
-        </div>
-    );
-}
->>>>>>> 31efdbd3e7f1193d837669c6fd06a2739110c151
 
 export default AdminPage;
