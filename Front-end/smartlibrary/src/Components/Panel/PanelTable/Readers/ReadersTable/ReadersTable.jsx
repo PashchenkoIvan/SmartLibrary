656f54b10b550f27452e54f03b88c13323542d17
KirillBazavlutskiy <bazik345@gmail.com>
2022-11-21 13:36:36 +0200

    changed panel components

---
+++ Front-end/smartlibrary/src/Components/Panel/PanelTable/Readers/ReadersTable/ReadersTable.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './ReadersTable.module.css';
+
+const ReadersTable = () => {
+	return (
+		<div className={s.container}>
+			<h1>hi</h1>
+		</div>
+	);
+};
+
+export default ReadersTable;
