b979a8fbc88cd66ba5149230f386228ee3bfb19d
ALEXSNIS <118630621+ALEXSNIS@users.noreply.github.com>
2022-11-22 16:12:01 +0200

    Update Navbar.jsx

--- Front-end/smartlibrary/src/containers/headerMainPage/Navbar/Navbar.jsx
+++ Front-end/smartlibrary/src/containers/headerMainPage/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import s from './Navbar.module.css'
-import { Play } from '../../../assets'
+import { Play } from '../../../assets/imgMainPage'
 
 const Navbar = () => {
 	return (
