0af70af9b508a8c3b3bd6e36fe94d6afd068a1a7
unknown <vanapasenko886@gmail.com>
2022-12-05 21:01:45 +0200

    dfv

---
+++ Front-end/smartlibrary/src/Components/TransHeader/TransHeader.jsx
@@ -0,0 +1,12 @@
+import s from './TransHeader.module.css';
+import React from 'react'
+
+const TransHeader = () => {
+  return (
+    <div className={s.container}>
+        
+    </div>
+  )
+}
+
+export default TransHeader;
