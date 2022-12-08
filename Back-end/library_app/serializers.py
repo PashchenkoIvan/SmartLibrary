244497a905c9bc4e34b40a8a49255782da875702
Sophirre <serik2005yt@gmail.com>
2022-12-08 15:05:56 +0200

    Book api get post methods

---
+++ Back-end/library_app/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from .models import Book
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        
+        fields = ('title',
+                'inventory_num',
+                'author',
+                'number_of_pages',
+                'publusher',
+                'isbn',
+                'book_type',
+                'language',
+                'cover_img_path',
+                'cover_type',
+                'category',
+                'description',
+                'time_to_read',
+                'pubication_date',
+                'pubication_year',
+                'city_of_publishing',
+                'ubk',
+                'additional_type',
+                'publication_type'
+                )
+        
+    
+class BookSerializerWithAdditional(serializers.ModelSerializer):
+
+    class Meta:
+        model = Book
+        fields = ("__all__")
