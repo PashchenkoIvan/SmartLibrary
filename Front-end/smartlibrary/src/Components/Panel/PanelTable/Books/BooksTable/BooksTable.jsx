52dafc46d670c9ddcb020537b211147d87e4b1dd
sadovenk0 <sadovenk0@icloud.com>
2022-12-08 10:09:54 +0200

    Added books counter & qr-popUp in Admin --> Books

--- Front-end/smartlibrary/src/Components/Panel/PanelTable/Books/BooksTable/BooksTable.jsx
+++ Front-end/smartlibrary/src/Components/Panel/PanelTable/Books/BooksTable/BooksTable.jsx
@@ -1,33 +1,65 @@
-import React, { useState } from 'react';
-import s from './BooksTable.module.css';
-import qrCode from '../../../img/qricon.png';
+import { React, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Popup from 'reactjs-popup';
+
+import s from './BooksTable.module.css';
+import sp from './SingleBook/popUps.module.css';
+import qrCode from '../../../img/qricon.png';
+import { QrIcon } from '../../../img';
 
 const BooksTable = props => {
 	const [filter, setFilter] = useState('all');
+	const [books, setBooks] = useState(props.books);
+	const [booksAmount, setBooksAmount] = useState(5);
 
-	let booksElements = props.books.map(b => {
-		let pathMain = `/book-single/${b.id}`;
-		let pathEdit = `/book-single/edit/${b.id}`;
-
-		return (
-			<div className={s.row}>
-				<Link to={pathMain} onClick={() => window.scrollTo(0, 0)}>{b.bookName}</Link>
-				<div>{b.bookAuthor}</div>
-				<div>{b.ISBN}</div>
-				<div>{b.isPopularBook ? 'У наявності' : 'Відсутня'}</div>
-				<div>
-					<img src={qrCode} alt='' />
+	const bookElementCreating = (b, counter) => {
+		if (counter < booksAmount) {
+		  	return (
+				<div className={s.row}>
+					<Link to={`/book-single/${b.id}`} onClick={() => window.scrollTo(0, 0)}>{b.bookName}</Link>
+					<div>{b.bookAuthor}</div>
+					<div>{b.ISBN}</div>
+					<div>{b.isPopularBook ? 'У наявності' : 'Відсутня'}</div>
+					<Popup
+							trigger={
+								<div>
+									<img src={qrCode} alt='' />
+								</div>
+							}
+							modal
+						>
+							{close => (
+								<>
+									<div className={sp.header}>
+									<span>{b.bookName}</span>
+									<button className={sp.closeBtn} onClick={close}>×</button>
+									</div>
+									<div className={sp.content}>
+										<img className={s.qrImg} src={QrIcon} alt={b.bookName} />
+										<button className={sp.btn} onClick={() => {}}>Роздрукувати</button>
+									</div>
+								</>
+							)}
+						</Popup>
+					<Link to={`/book-single/edit/${b.id}`} onClick={() => window.scrollTo(0, 0)}>Редагувати</Link>
 				</div>
-				<Link to={pathEdit} onClick={() => window.scrollTo(0, 0)}>Редагувати</Link>
-			</div>
-		);
-	});
+		  	)
+		} else { }
+	}
+
+	let booksCounter = 0;
+	let booksElements = books.map(el => { booksCounter = 0; return el; }).map((b) => (bookElementCreating(b, booksCounter++)))
+
+	useEffect(
+		() => {
+		  	setBooks(props.books);
+		}, [props.books],
+	);
 
 	return (
 		<div className={s.container}>
 			<div className={s.filter}>
-				<p>Загальна кількість книг</p>
+				<p>Загальна кількість книг {`(${props.books.length})`}</p>
 				<div className={s.filters}>
 					<label className={s.filterItem} htmlFor='all'>
 						<input
@@ -70,13 +102,16 @@ const BooksTable = props => {
 				</div>
 			</div>
 			<div className={s.header}>
-				<p>Назва книги</p>
+				<p>Назва книги {`(${booksAmount > books.length ? books.length : booksAmount})`}</p>
 				<p>Автор</p>
 				<p>Інвертарний номер</p>
 				<p>Статус</p>
 				<p>QR-код</p>
 			</div>
 			<div className={s.main}>{booksElements}</div>
+			<button className={s.btn} onClick={() => setBooksAmount((c) => c + 4)}>
+                Показати ще
+            </button>
 		</div>
 	);
 };
