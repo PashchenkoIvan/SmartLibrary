af045cd39f72c533bfe13a7dacc54da1810f0e40
kirill bazavlutskiy <bazik345@gmail.com>
2022-12-14 21:58:49 +0200

    fixed styles in headers

--- Front-end/smartlibrary/src/Components/TransHeader/TransHeader.jsx
+++ Front-end/smartlibrary/src/Components/TransHeader/TransHeader.jsx
@@ -5,32 +5,11 @@ import sample from './video/videoplayback.mp4';
 const TransHeader = () => {
 	return (
 		<div className={s.container}>
-			{/* <video className={s.videoTag} autoPlay loop muted>
-          <source src={sample} type='video/mp4' />
-      </video>
-      <div className={s.blueFilter}>
-        <div className={s.centerBlock}>
-          <h1>Вітаємо, раді вас бачити</h1>
-          <p>Бібліотека Новоолександрівської сільської ради Філія №1</p>
-          <div className={s.inputBlock}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" >
-              <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-            </svg>
-            <input type="text" placeholder='Введіть назву книги або автора'/>
-            <button></button>
-          </div>
-        </div>
-      </div> */}
-			<video className={s.videoTag} autoPlay loop muted>
-				<source src={sample} type='video/mp4' />
-			</video>
-			<div className={s.blueFilter} />
 			<div className={s.inner}>
-				<div className={s.logo}></div>
+				<Link to='/' className={s.link}>
+					<div className={s.logo}></div>
+				</Link>
 				<div className={s.navlinksBlock}>
-					<Link to='/' className={s.link}>
-						Головна
-					</Link>
 					<Link to='/catalog' className={s.link}>
 						Каталог книг
 					</Link>
@@ -49,25 +28,29 @@ const TransHeader = () => {
 					</Link>
 				</div>
 			</div>
+			<video className={s.videoTag} autoPlay loop muted>
+				<source src={sample} type='video/mp4' />
+			</video>
+			<div className={s.blueFilter} />
 			<div className={s.headerSearch}>
 				<h1>Вітаємо, раді вас бачити</h1>
 				<p>Бібліотека Новоолександрівської сільської ради Філія №1</p>
 				<div className={s.inputBlock}>
 					<div className={s.searchIcon}>
 						<svg
-								xmlns='http://www.w3.org/2000/svg'
-								fill='none'
-								viewBox='0 0 24 24'
-								stroke-width='1.5'
-							>
-								<path
-									stroke-linecap='round'
-									stroke-linejoin='round'
-									d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
-								/>
+							xmlns='http://www.w3.org/2000/svg'
+							fill='none'
+							viewBox='0 0 24 24'
+							stroke-width='1.5'
+						>
+							<path
+								stroke-linecap='round'
+								stroke-linejoin='round'
+								d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+							/>
 						</svg>
 					</div>
-					<input type="text" placeholder='Введіть назву книги або автора'/>
+					<input type='text' placeholder='Введіть назву книги або автора' />
 					<button>Шукати</button>
 				</div>
 			</div>
