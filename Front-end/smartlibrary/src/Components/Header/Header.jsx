293663cbf3d5c3f69d894c3364c4145d1c960363
kirill bazavlutskiy <bazik345@gmail.com>
2022-12-18 21:30:48 +0200

    changed burger menu in headers

--- Front-end/smartlibrary/src/Components/Header/Header.jsx
+++ Front-end/smartlibrary/src/Components/Header/Header.jsx
@@ -1,44 +1,51 @@
 import s from './Header.module.css';
 import React, { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
 	const menu = useRef(null);
 	const [menuActive, setMenuActive] = useState(false);
 
 	useEffect(() => {
-		menu.current.style.top = '-240px';
+		menu.current.style.top = 'calc(-100vh + 100px)';
 	}, []);
 
 	useEffect(() => {
-		let start = -140,
-			end = 100;
+		let start = -100,
+			end = 0;
 		let menuStyle = menu.current.style;
 		if (menuActive) {
 			let i = start;
 			let showMenu = setInterval(() => {
-				if (menuStyle.top == `${end}px`) clearInterval(showMenu);
+				if (menuStyle.top == `calc(${end}vh + 100px)`) clearInterval(showMenu);
 				else {
-					menuStyle.top = i + 'px';
-					i += 10;
+					menuStyle.top = `calc(${i}vh + 100px)`;
+					i += 2;
 				}
-			});
+			}, 4);
 		} else {
 			let i = end;
 			let hideMenu = setInterval(() => {
-				if (menuStyle.top == `${start}px`) clearInterval(hideMenu);
+				if (menuStyle.top == `calc(${start}vh + 100px)`)
+					clearInterval(hideMenu);
 				else {
-					menuStyle.top = i + 'px';
-					i -= 10;
+					menuStyle.top = `calc(${i}vh + 100px)`;
+					i -= 2;
 				}
-			});
+			}, 4);
 		}
 	}, [menuActive]);
+
 	return (
 		<div className={s.container}>
 			<div className={s.background} />
 			<div className={s.inner}>
-				<Link to='/'>
+				<Link
+					className={navData =>
+						(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+					}
+					to='/'
+				>
 					<div className={s.logo}></div>
 				</Link>
 				<div className={s.navlinksBlock}>
@@ -48,15 +55,13 @@ const Header = () => {
 					<Link to='/contacts' className={s.link}>
 						Контакти
 					</Link>
-					<Link to='./faq/making-orders' className={s.link}>
+					<Link to='/faq/making-orders' className={s.link}>
 						Як це працює
 					</Link>
 					<Link to='/admin/readers' className={s.link}>
 						Бібліотекар
 					</Link>
-					<Link to='' className={s.link}>
-						Вийти
-					</Link>
+					<Link className={s.link}>Вийти</Link>
 					<Link to='/personPage' className={s.blueLink}>
 						Особистий кабінет
 					</Link>
@@ -77,23 +82,61 @@ const Header = () => {
 				</button>
 			</div>
 			<div className={s.menu} ref={menu}>
-				<div className={s.navlinksBlockMenu}>
-					<Link to='/catalog' className={s.link}>
+				<div className={s.columnMenu}>
+					<NavLink
+						className={navData =>
+							(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+						}
+						to='/catalog'
+					>
 						Каталог книг
-					</Link>
-					<Link to='/contacts' className={s.link}>
+					</NavLink>
+					<NavLink
+						className={navData =>
+							(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+						}
+						to='/contacts'
+					>
 						Контакти
-					</Link>
-					<Link to='/faq/making-orders' className={s.link}>
+					</NavLink>
+					<NavLink
+						className={navData =>
+							(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+						}
+						to='./faq/making-orders'
+					>
 						Як це працює
-					</Link>
-					<Link to='/admin/readers' className={s.link}>
+					</NavLink>
+					<NavLink
+						className={navData =>
+							(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+						}
+						to='/admin/readers'
+					>
 						Бібліотекар
-					</Link>
-					<Link className={s.link}>Вийти</Link>
-					<Link to='/personPage' className={s.blueLink}>
+					</NavLink>
+					<NavLink
+						className={navData =>
+							(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+						}
+						to=''
+					>
+						Вийти
+					</NavLink>
+					<NavLink
+						className={navData =>
+							(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+						}
+						to='/personPage'
+					>
 						Особистий кабінет
-					</Link>
+					</NavLink>
+				</div>
+				<div className={s.contacts}>
+					<a href='tel:+380932226111'>+38 093 22 26 111</a>
+					<a href='https://www.google.com/maps/place/%D0%B2%D1%83%D0%BB.+%D0%9D%D0%B0%D1%83%D0%BA%D0%BE%D0%B2%D0%B0,+20,+%D0%94%D0%BE%D1%81%D0%BB%D1%96%D0%B4%D0%BD%D0%B5,+%D0%94%D0%BD%D1%96%D0%BF%D1%80%D0%BE%D0%BF%D0%B5%D1%82%D1%80%D0%BE%D0%B2%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+52071/@48.3872864,35.0324823,17z/data=!3m1!4b1!4m5!3m4!1s0x40dbfb13c13f5433:0x3937755b663960b6!8m2!3d48.3872864!4d35.0324823'>
+						<p>c. Дослідне вул. Наукова 20</p>
+					</a>
 				</div>
 			</div>
 		</div>
--- Front-end/smartlibrary/src/Components/TransHeader/TransHeader.jsx
+++ Front-end/smartlibrary/src/Components/TransHeader/TransHeader.jsx
@@ -1,5 +1,5 @@
 import s from './TransHeader.module.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import sample from './video/videoplayback.mp4';
 import { useEffect, useRef, useState } from 'react';
 
@@ -8,29 +8,31 @@ const TransHeader = () => {
 	const [menuActive, setMenuActive] = useState(false);
 
 	useEffect(() => {
-		menu.current.style.top = '-340px';
+		menu.current.style.top = '-100vh';
 	}, []);
 
 	useEffect(() => {
+		let start = -100,
+			end = 0;
 		let menuStyle = menu.current.style;
 		if (menuActive) {
-			let i = -340;
+			let i = start;
 			let showMenu = setInterval(() => {
-				if (menuStyle.top == '0px') clearInterval(showMenu);
+				if (menuStyle.top == `${end}vh`) clearInterval(showMenu);
 				else {
-					menuStyle.top = i + 'px';
-					i += 10;
+					menuStyle.top = `${i}vh`;
+					i += 2;
 				}
-			});
+			}, 4);
 		} else {
-			let i = 0;
+			let i = end;
 			let hideMenu = setInterval(() => {
-				if (menuStyle.top == '-340px') clearInterval(hideMenu);
+				if (menuStyle.top == `${start}vh`) clearInterval(hideMenu);
 				else {
-					menuStyle.top = i + 'px';
-					i -= 10;
+					menuStyle.top = `${i}vh`;
+					i -= 2;
 				}
-			});
+			}, 4);
 		}
 	}, [menuActive]);
 
@@ -73,30 +75,68 @@ const TransHeader = () => {
 					</svg>
 				</button>
 			</div>
+			<div className={s.menu} ref={menu}>
+				<div className={s.header}></div>
+				<div className={s.columnMenu}>
+					<NavLink
+						className={navData =>
+							(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+						}
+						to='/catalog'
+					>
+						Каталог книг
+					</NavLink>
+					<NavLink
+						className={navData =>
+							(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+						}
+						to='/contacts'
+					>
+						Контакти
+					</NavLink>
+					<NavLink
+						className={navData =>
+							(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+						}
+						to='./faq/making-orders'
+					>
+						Як це працює
+					</NavLink>
+					<NavLink
+						className={navData =>
+							(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+						}
+						to='/admin/readers'
+					>
+						Бібліотекар
+					</NavLink>
+					<NavLink
+						className={navData =>
+							(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+						}
+						to=''
+					>
+						Вийти
+					</NavLink>
+					<NavLink
+						className={navData =>
+							(navData.isActive ? s.isActive : s.notActive) + ' ' + s.item
+						}
+						to='/personPage'
+					>
+						Особистий кабінет
+					</NavLink>
+				</div>
+				<div className={s.contacts}>
+					<a href='tel:+380932226111'>+38 093 22 26 111</a>
+					<a href='https://www.google.com/maps/place/%D0%B2%D1%83%D0%BB.+%D0%9D%D0%B0%D1%83%D0%BA%D0%BE%D0%B2%D0%B0,+20,+%D0%94%D0%BE%D1%81%D0%BB%D1%96%D0%B4%D0%BD%D0%B5,+%D0%94%D0%BD%D1%96%D0%BF%D1%80%D0%BE%D0%BF%D0%B5%D1%82%D1%80%D0%BE%D0%B2%D1%81%D1%8C%D0%BA%D0%B0+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C,+52071/@48.3872864,35.0324823,17z/data=!3m1!4b1!4m5!3m4!1s0x40dbfb13c13f5433:0x3937755b663960b6!8m2!3d48.3872864!4d35.0324823'>
+						<p>c. Дослідне вул. Наукова 20</p>
+					</a>
+				</div>
+			</div>
 			<video className={s.videoTag} autoPlay loop muted>
 				<source src={sample} type='video/mp4' />
 			</video>
-			<div className={s.menu} ref={menu}>
-				<div className={s.header}></div>
-				<div className={s.navlinksBlockMenu}>
-					<Link to='/catalog' className={s.link}>
-						Каталог книг
-					</Link>
-					<Link to='/contacts' className={s.link}>
-						Контакти
-					</Link>
-					<Link to='/faq/making-orders' className={s.link}>
-						Як це працює
-					</Link>
-					<Link to='/admin/readers' className={s.link}>
-						Бібліотекар
-					</Link>
-					<Link className={s.link}>Вийти</Link>
-					<Link to='/personPage' className={s.blueLink}>
-						Особистий кабінет
-					</Link>
-				</div>
-			</div>
 			<div className={s.blueFilter} />
 			<div className={s.headerSearch}>
 				<h1>Вітаємо, раді вас бачити</h1>
