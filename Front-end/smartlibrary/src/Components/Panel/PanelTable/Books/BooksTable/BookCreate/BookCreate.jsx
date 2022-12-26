8a3a79f1768d09235985edc830c436950706f966
sadovenk0 <sadovenk0@icloud.com>
2022-12-26 14:56:03 +0200

    Bag fixing on AddBook Page

--- Front-end/smartlibrary/src/Components/Panel/PanelTable/Books/BooksTable/BookCreate/BookCreate.jsx
+++ Front-end/smartlibrary/src/Components/Panel/PanelTable/Books/BooksTable/BookCreate/BookCreate.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Form } from '../../../../../index';
 
 import s from './bookCreate.module.css';
@@ -7,6 +7,7 @@ import additionalFieldsData from '../additionalFieldsData';
 
 const BookCreate = props => {
 	props.setHeader(false)
+	const navigate = useNavigate();
 
 	return (
 		<div className={s.container}>
@@ -25,7 +26,26 @@ const BookCreate = props => {
 						placeholder='Введіть штрих код книги'
 					/>
 				</div>
-				<Form main={MainlFieldsData({})} additional={additionalFieldsData({})} btns={[{title: "Додати книгу", type: "submit", post: "add-book"}]} />
+				<Form
+					main={MainlFieldsData({})}
+					additional={additionalFieldsData({})}
+					btns={
+						[
+							{
+								title: "Додати книгу",
+								type: "submit",
+								post: "add-book",
+								onclick: () => 
+									setTimeout(
+										() => {
+											navigate('/admin/books')
+											window.scrollTo(0, 0)
+										}, 100
+									),
+							}
+						]
+					}
+				/>
 			</div>
 		</div>
 	);
