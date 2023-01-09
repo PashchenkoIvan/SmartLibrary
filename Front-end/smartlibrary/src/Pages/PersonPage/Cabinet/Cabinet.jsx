422a3b164366886fead54519630a1678ed1c6c96
Kirill <bazik345@gmail.com>
2023-01-09 16:45:56 +0200

    adaptation for person page (is not finished yet)

--- Front-end/smartlibrary/src/Pages/PersonPage/Cabinet/Cabinet.jsx
+++ Front-end/smartlibrary/src/Pages/PersonPage/Cabinet/Cabinet.jsx
@@ -19,26 +19,28 @@ const Cabinet = () => {
 		<div className={s.container}>
 			<p className={s.cabinetTitle}>ОСОБИСТИЙ КАБІНЕТ</p>
 			<div className={s.block}>
-				<div className={s.qrBlock}>
-					<p>Особисттий QR-код</p>
-					<div className={s.qrCode}>
-						<img src={Qr} alt='Qr-code' />
+				<div className={s.qrAndEdit}>
+					<div className={s.qrBlock}>
+						<p>Особисттий QR-код</p>
+						<div className={s.qrCode}>
+							<img src={Qr} alt='Qr-code' />
+						</div>
 					</div>
-				</div>
-				<div className={s.editBlock}>
-					<p>Pashchenko Ivan</p>
-					<div className={s.buttonBlock}>
-						<Popups />
-						<Link
-							to='/'
-							className={s.link}
-							onClick={() => {
-								Services.AuthService.Logout();
-								return this.forceUpdate();
-							}}
-						>
-							Вийти з системи
-						</Link>
+					<div className={s.editBlock}>
+						<p>Pashchenko Ivan</p>
+						<div className={s.buttonBlock}>
+							<Popups />
+							<Link
+								to='/'
+								className={s.link}
+								onClick={() => {
+									Services.AuthService.Logout();
+									return this.forceUpdate();
+								}}
+							>
+								Вийти з системи
+							</Link>
+						</div>
 					</div>
 				</div>
 				<GCalendar />
