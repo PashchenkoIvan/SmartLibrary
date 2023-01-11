0e4ed698e9ebb9cacecdc12e859c07d0628c1348
kirill bazavlutskiy <bazik345@gmail.com>
2023-01-11 08:43:50 +0200

    fixed adaptation in main page and footer

--- Front-end/smartlibrary/src/Components/Footer/Footer.jsx
+++ Front-end/smartlibrary/src/Components/Footer/Footer.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import s from './Footer.module.css'
-import ContClock from './ContItem/ContClock'
-import ContLogo from './ContItem/ContLogo'
-import ContPhone from './ContItem/ContPhone'
-import ContPoint from './ContItem/ContPoint'
+import React from 'react';
+import s from './Footer.module.css';
+import ContClock from './ContItem/ContClock';
+import ContLogo from './ContItem/ContLogo';
+import ContPhone from './ContItem/ContPhone';
+import ContPoint from './ContItem/ContPoint';
 
 const Footer = () => {
 	return (
 		<footer className={s.footer}>
 			<div className={s.footerContainer}>
-				<ContClock />
-				<ContPhone />
-				<ContPoint />
+				<div className={s.footerBlocks}>
+					<ContClock />
+					<ContPhone />
+					<ContPoint />
+				</div>
 				<ContLogo />
 			</div>
 		</footer>
-	)
-}
+	);
+};
 
 export default Footer;
