2b955b799c7cf6b678a9c37cf9cf1eddc545d49d
Kirill <bazik345@gmail.com>
2023-01-12 21:59:04 +0200

    adaptation fixes

--- Front-end/smartlibrary/src/Pages/PersonPage/Cabinet/Cabinet.jsx
+++ Front-end/smartlibrary/src/Pages/PersonPage/Cabinet/Cabinet.jsx
@@ -7,10 +7,10 @@ import Popups from './Popups/Popups';
 import GCalendar from '../../../Components/GlobalCalendar/GCalendar';
 
 import s from './Cabinet.module.css';
-import sp from '../../../assets/styles/popUp.module.css';
 
 import Qr from '../image/qrcode.png';
-import Popup from 'reactjs-popup';
+
+// import 'react-calendar/dist/Calendar.css';
 
 const Cabinet = () => {
 	const Services = useContext(ServicesContext);
@@ -21,24 +21,9 @@ const Cabinet = () => {
 			<div className={s.block}>
 				<div className={s.qrAndEdit}>
 					<div className={s.qrBlock}>
-						<p>Особисттий QR-код</p>
+						<p>Особисттий QR-код</p>ы
 						<div className={s.qrCode}>
 							<img src={Qr} alt='Qr-code' />
-							<Popup trigger={<button>Показати QR-код</button>} modal>
-								{close => (
-									<>
-										<div className={sp.header}>
-											<span>Проскануйте QR-код</span>
-											<button className={sp.closeBtn} onClick={close}>
-												×
-											</button>
-										</div>
-										<div className={sp.content}>
-											<img src={Qr} alt='Qr-code' />
-										</div>
-									</>
-								)}
-							</Popup>
 						</div>
 					</div>
 					<div className={s.editBlock}>
